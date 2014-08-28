99d8e1ccd60473a4e11af8fd68f66beefc7628ff
timsvoice <timsethvoice@gmail.com>
2014-08-28 14:44:55 -0400

    Create carrierwave.rb
    Sample carrierwave.rb file

---
+++ config/carrierwave.rb
@@ -0,0 +1,21 @@
+CarrierWave.configure do |config|
+
+  config.cache_dir = File.join(Rails.root, 'tmp', 'uploads')
+
+  case Rails.env.to_sym
+
+  when :development
+    config.storage = :file
+    config.root = File.join(Rails.root, 'public')
+
+  when :production
+    config.storage = :file
+    config.root = File.join(Rails.root, 'public')
+
+  else
+    # settings for the local filesystem
+    config.storage = :file
+    config.root = File.join(Rails.root, 'public')
+  end
+
+end
